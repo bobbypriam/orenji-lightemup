700abb4e61514ece1d34c031ff138b55cf7dddae
Bobby Priambodo <bobby.priambodo@gmail.com>
2014-12-11 15:10:11 +0700

    fix bug program terminate after answer

--- server.py
+++ server.py
@@ -6,8 +6,9 @@ import thread
 import time
 import threading
 
-HOST = ''
-PORT = 8888
+# HOST = '192.168.42.1'
+HOST = 'localhost'
+PORT = 10000
 
 n = 0
 t = 0
@@ -27,6 +28,52 @@ def giveturn(conn):
 
 	conn.send(msg)
 
+# client thread function
+def clientthread(conn, n):
+	lastreq = ''
+	while True:
+		msg_str = conn.recv(1024)
+
+		global quest
+		global ans
+		global turn
+
+		if msg_str != lastreq:
+			print "CLIENT " + str(n) + " ASK " + msg_str
+			lastreq = msg_str
+
+		if (msg_str == "AREDONE"):
+			if (quest == 'undefined'):
+				conn.send("NOPE")
+			else:
+				conn.send("GO")
+
+		elif (msg_str == "NEEDRESULT"):
+			if (quest == 'undefined'):
+				conn.send("NOQUEST")
+			elif (ans == 'undefined'):
+				conn.send('WAIT')
+			else:
+				print "Result:",
+				if (quest == ans):
+					print "menang"
+					conn.send("MENANG")
+				else:
+					print "kalah"
+					conn.send("KALAH")
+
+				time.sleep(1)
+				quest = 'undefined'
+				ans = 'undefined'
+
+		else:
+			if (quest == 'undefined'):
+				quest = msg_str
+				print "Quest:", quest
+			elif (ans == 'undefined'):
+				ans = msg_str
+				print "Answer:", ans 
+
 # main program starts here
 if __name__ == "__main__":
 
@@ -35,67 +82,14 @@ if __name__ == "__main__":
 		# create socket
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-		youstupid = True
-		while youstupid:
-			try:
-				print "Please insert host number:",
-				HOST = raw_input()
-				print "Please insert port number:",
-				PORT = int(raw_input())
-				# bind
-				s.bind((HOST, PORT))
-				youstupid = False
-			except socket.error, msg:
-				if msg[0] == 13:
-					print "Error: Port is unusable.\n"
-				elif msg[0] == 99:
-					print "Error: Cannot assign requested host.\n"
-			except ValueError:
-				print "Error: Port must be a number.\n"
+		
+		# bind
+		s.bind((HOST, PORT))
 
 		# listen
 		s.listen(10)
 		print 'Server opened on on host', HOST, 'port ' + str(PORT)
 		print 'Server ready, now listening ...'
-
-		# client thread function
-		def clientthread(conn):
-			while True:
-				msg_str = conn.recv(1024)
-
-				global quest
-				global ans
-				print quest
-				print ans
-
-				print "CLIENT ASK " + msg_str
-
-				if (msg_str == "AREDONE"):
-					if (quest == 'undefined'):
-						conn.send("NOPE")
-					else:
-						conn.send("GO")
-
-				elif (msg_str == "NEEDRESULT"):
-					if (quest == 'undefined'):
-						conn.send("NOQUEST")
-					elif (ans == 'undefined'):
-						conn.send('WAIT')
-					else:
-						if (quest == ans):
-							conn.send("MENANG")
-						else:
-							conn.send("KALAH")
-
-						quest = 'undefined'
-						ans = 'undefined'
-
-				else:
-					if (quest == 'undefined'):
-						quest = msg_str
-					else:
-						ans = msg_str
 
 		# response request
 		while True:
@@ -106,8 +100,15 @@ if __name__ == "__main__":
 			giveturn(conn)
 
 			# create thread 
-			thread.start_new_thread(clientthread, (conn,))
+			thread.start_new_thread(clientthread, (conn,n))
 
+	except socket.error, msg:
+		if msg[0] == 13:
+			print "Error: Port is unusable.\n"
+		elif msg[0] == 99:
+			print "Error: Cannot assign requested host.\n"
+	except ValueError:
+		print "Error: Port must be a number.\n"
 	except KeyboardInterrupt:
 		print('\nKeyboard interrupt detected. Exiting gracefully...\n')
 		quit()
